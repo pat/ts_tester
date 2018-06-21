40b564c4c1a13812b1d7384175429d9d19a41544
Pat Allan <pat@freelancing-gods.com>
2018-06-21 08:26:43 +0700

    Major gem updates.

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130729074547) do
+ActiveRecord::Schema.define(version: 2013_07_29_074547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "articles", force: true do |t|
-    t.string   "title"
-    t.text     "body"
+  create_table "articles", id: :serial, force: :cascade do |t|
+    t.string "title", limit: 255
+    t.text "body"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
